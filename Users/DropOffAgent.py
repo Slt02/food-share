4c7bd8ac86beb04040d7a4a77755fea4ee0980b8
IOANNIS SOULIOTIS <giannissouliotis2002@gmail.com>
2025-05-16 21:49:30 +0300

    Folder creation and DropOffAgent Class

---
+++ Users/DropOffAgent.py
@@ -0,0 +1,7 @@
+from Users.User import User
+
+# Child class of User Class
+class DropOffAgent(User):
+    def __init__(self, username, name, surname, password, email, phone_number, deliveries_taken):
+        super().__init__(username, name, surname, password, email, phone_number)
+        self.deliveries_taken = [] # List of deliveries taken by the agent
