657ff7f5792445a7a90ad93a709435cdc95486f2
merkosmarios <merkosmar@gmail.com>
2025-05-21 15:15:13 +0300

    .

--- Delivery.py
+++ Delivery.py
@@ -1,6 +1,5 @@
 class Delivery:
-    def __init__ (self, order_id, Drop_off_Agent_id, customer_id, customer_address):
-        self.order_id = order_id
+    def __init__ (self, Drop_off_Agent_id, customer_id, customer_address):
         self.Drop_off_Agent_id = Drop_off_Agent_id
         self.customer_id = customer_id
         self.customer_address = customer_address
