8c08988364618bb8287dbcdc246f13dbf1dff98e
dionysispefanis <dionysispefanis@gmail.com>
2025-05-29 15:36:04 +0300

    Updated Customer Class

--- Users/Customer.py
+++ Users/Customer.py
@@ -4,4 +4,4 @@ class Customer(User):
     def __init__(self, username, name, surname, password, email, phone_number, customer_id=None):
         super().__init__(customer_id, username, name, surname, password, email)
         self.phone_number = phone_number
-        self.order_history = []
+        
