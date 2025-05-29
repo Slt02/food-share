83304c5120956aed6b3a5d8992576326995d6f17
dionysispefanis <dionysispefanis@gmail.com>
2025-05-29 15:09:35 +0300

    Updated Admin class

--- Users/Admin.py
+++ Users/Admin.py
@@ -4,6 +4,4 @@ class Admin(User):
     def __init__(self, username, name, surname, password, email, admin_id=None):
         super().__init__(admin_id, username, name, surname, password, email)
 
-    def create_drop_off_acc(self):
-        # TO-DO Function to create a new drop off agent
-        pass
+    
